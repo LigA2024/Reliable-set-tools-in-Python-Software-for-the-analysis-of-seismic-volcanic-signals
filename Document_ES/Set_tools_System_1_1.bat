8bb4688f85e8299d30e16661f11369672a7e45fa
Ligdamis Gutiérrez Espinoza <169895412+LigA2024@users.noreply.github.com>
2025-12-04 11:34:06 +0100

    Add files via upload

---
+++ Document_ES/Set_tools_System_1_1.bat
@@ -0,0 +1,39 @@
+@echo off
+
+REM "Reliable set tools in Python Software for the analysis of seismic-volcanic signals. Vers. 1.1"
+REM  "Author: PhD Ligdamis A. Gutiérrez E. PhD."
+REM  "2021-2023"
+Title Reliable set tools in Python Software for the analysis of seismic-volcanic signals. (Vers. 1.1).
+color 1F
+echo.
+echo.
+echo             	=================================================
+echo             	=                                               =
+echo             	=      Welcome to:                              =
+echo             	=                                               =
+echo             	=  Reliable set tools in Python Software for    =
+echo 		=  the analysis of seismic-volcanic signals     =
+echo                 =                                               =
+echo                 =  Theoretical and Cosmos Physics Department.   = 
+echo                 =                Science Faculty                =
+echo             	=      Andalusian Institute of Geophysics       =
+echo             	=            Granada University (Ugr)           =
+echo             	=                                               =
+echo             	=          Developed in Python 3.8.6            =
+echo             	=     Execution for Windows and Linux System    =
+echo             	=                 2021 -2023                    =
+echo             	=================================================
+echo.            
+echo        Please, Minimize the CMD from the Python.exe window (in black).
+echo            (Don't close it, because the system also closes).           
+echo        When you exit the system, this window closes automatically. 
+echo.           
+echo          	    To Continue, please press a key.
+echo.
+echo               *** Please, press a key to start the System. ***        
+echo.
+pause>nul
+
+CD "%USERPROFILE%\Documents\Reliable_set_tools_system_1\"
+cls
+START python Menu.py
